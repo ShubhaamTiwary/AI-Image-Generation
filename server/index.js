efbe57e83f9a6f54c8139e1cf3e688591e01b62e
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-06-09 12:39:11 +0530

    Connected Atlas + Created Post Model

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from "./mongodb/connect.js";
 
 dotenv.config();
 
@@ -9,15 +10,16 @@ app.use(cors());
 app.use(express.json({limit:'50mb'}));
 
 app.get('/', async (req,res) =>{
-    res.send("Dall-E!!")
+    res.send("Dall-E!!");
 });
 
 const startServer = async () => {
     try {
+        connectDB(process.env.MONGODB_URL);
         app.listen(8080, () => console.log('Server started on port 8080'));
     } catch (error) {
         console.log(error);
     }
 };
-  
-  startServer();
+
+startServer();
