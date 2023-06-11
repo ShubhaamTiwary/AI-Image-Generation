33dc9a20ab83651146fb1bdc807a6afa0689cb37
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-06-11 10:46:17 +0530

    Showing Card at Home Page

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import {donwload} from '../assets';
+import {downloadImage} from '../utils'
 
-const Card = () => {
+
+const Card = ({_id,name,prompt,photo}) => {
   return (
     <div>Card</div>
   )
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -18,6 +18,33 @@ const Home = () => {
   const [allPosts,setAllPosts] = useState(null);
   const [searchText,setSearchText] =  useState("");
 
+
+  const fetchPosts = async () => {
+    setLoading(true);
+
+    try {
+      const response = await fetch('http://localhost:8080/api/v1/post', {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (response.ok) {
+        const result = await response.json();
+        setAllPosts(result.data.reverse());
+      }
+    } catch (err) {
+      alert(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
   return (
     <section className='max-w-7xl mx-auto'>
       <div>
@@ -47,12 +74,12 @@ const Home = () => {
               <div className='grid lg:grid-cols-4 sm-grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3'>
                 {searchText?(
                   <RenderCards 
-                    data={[]}
+                    data={allPosts}
                     title="No Search Results Found"
                   />
                 ):(
                   <RenderCards
-                    data={[]}
+                    data={allPosts}
                     title="No posts found"
                   />
                 )}
