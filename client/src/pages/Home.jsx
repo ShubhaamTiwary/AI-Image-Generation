84864f6bdfe4c2b2dc37d91ab9b72fe9ea2126ab
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-06-11 12:36:15 +0530

    Updated to Deployed URL

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://ai-image-generation-y4z8.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
